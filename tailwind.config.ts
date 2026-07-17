82eeec916fe113e652dd3551b3fd368994cf7027
teamskiolo-maker <teamskiolo@gmail.com>
2026-07-17 16:01:31 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,15 @@ const config: Config = {
       },
       letterSpacing: {
         tight2: "-0.02em"
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" }
+        }
+      },
+      animation: {
+        marquee: "marquee 40s linear infinite"
       }
     },
   },
